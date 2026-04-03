e8d56efacf7fff7fc51552a1acd48a4b55732534
Fliss <fliss.douglas@collinsongroup.com>
2026-04-03 20:46:58 +0100

    add new quick sort function

--- quick-sort.js
+++ quick-sort.js
@@ -1,3 +1,4 @@
+// Version creating new arrays 
 function quickSort(arr) {
     // Base case: arrays have 1 or less element are sorted 
     if (arr.length <= 1) {
@@ -9,6 +10,7 @@ function quickSort(arr) {
     const pivotIndex = Math.floor(arr.length / 2)
     const pivot = arr[pivotIndex];
 
+    // Each time quickSort runs it creates 2 new arrays
     const left = [];
     const right = [];
 
@@ -32,3 +34,37 @@ function quickSort(arr) {
 console.log(quickSort([4, 10, 7, 3, 5, 12, 40, 1, 33]));
 console.log(quickSort([4, 8, 3, 1, 7, 3]));
 console.log(quickSort([6, 5, 4, 3, 2, 1]));
+
+
+// Version editing array in place 
+function quickSortInPlace(arr, low = 0, high = arr.length -1) {
+    // Base case: when indexes meet, array is sorted
+    if (low >= high) return; 
+
+    const pivot = arr[high]; // Pivot as last index of arr
+    let i = low - 1; // Index tracks behind and swaps values when needed
+
+    for (let j = low; j < high; j++) {
+        // If the value of j is less than the pivot 
+        if (arr[j] <= pivot) {
+            // Increment i so it tracks behind j
+            i++;
+            // Swap values of i and j
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+        // Swap pivot value so it's in right position 
+        [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+        // Store pivot index to use as low/high parameter in next function calls
+        const pivotIndex = i + 1;
+
+        // Recursively sort left and right partitions
+        quickSortInPlace(arr, low, pivotIndex - 1);
+        quickSortInPlace(arr, pivotIndex + 1, high);
+
+        return arr;
+}
+
+console.log(quickSortInPlace([4, 10, 7, 3, 5, 12, 40, 1, 33]));
+console.log(quickSortInPlace([4, 8, 3, 1, 7, 3]));
+console.log(quickSortInPlace([6, 5, 4, 3, 2, 1]));
